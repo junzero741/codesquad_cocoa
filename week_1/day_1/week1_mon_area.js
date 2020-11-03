f5df4e7fc67e8db4d39b31d2f7fe3ac1c5a39c55
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-11-03 16:20:04 +0900

    원의 넓이 합 출력 오류 수정 + 크롱의 코드 피드백 반영

--- week_1/day_1/week1_mon_area.js
+++ week_1/day_1/week1_mon_area.js
@@ -1,56 +1,57 @@
-var memory = [];
-var resultMemory = [];
-
-function getArea(object, value1, value2, value3) {
-
-    var sum = 0;
-    var answer;
-   
-
-    if(object === 'circle'){
-
-        answer = (value1 * value1) * Math.PI;
-
-          while(value1 <= value2) {
-            answer += answer;
-            value1++;
-        }
-        console.log(answer);
-    }
-
-
-    if(object === 'rect'){
-        answer = value1 * value2;
-        console.log(answer);
-    }
-
-    if(object === 'trapezoid'){
-        answer = (value1 + value2) * value3 / 2;
-        console.log(answer);
-    }
-
-    memory.push("getArea");
-    resultMemory.push(answer);
-  
+memoryObj = {
+    memory : [],
+    resultMemory : []
 }
 
-getArea('circle', 2);
-getArea('rect', 2, 3);
-getArea('trapezoid', 10, 15, 12);
-getArea('circle', 2, 5);
-printExecutionSequence()
 
+function getArea(polygon, value1, value2, value3) {
 
-function getCircle() {
-    const CIRCLE = 10;
+    let answer;
+    let sum = 0;
+   
+    if(polygon === 'circle'){
+
+        answer = (value1 * value1) * Math.PI;   //12.56         40.82
+        
+
+          while(value1 <= value2) {
+            answer = (value1 * value1) * Math.PI;
+            sum += answer;                   // 12.56 + 28.26    
+            value1++;                           // 2 -> 3
+        }
+    }
+
+    if(polygon === 'rect'){
+        answer = value1 * value2;
+    }
+
+    if(polygon === 'trapezoid'){
+        answer = (value1 + value2) * value3 / 2;
+    }
+
+    if(sum != 0) {
+        answer = sum;
+    }
+    
+    memoryObj.memory.push("getArea");
+    memoryObj.resultMemory.push(answer);
+  
 }
 
 
 function printExecutionSequence() {
 
-        memory.push("printExecutionSequence")
+        memoryObj.memory.push("printExecutionSequence")
 
-    for(var i = 0; i < memory.length; i++){
-        console.log(memory[i] + "result : " + resultMemory[i]);
+    for(var i = 0; i < memoryObj.memory.length; i++){
+        console.log("function : " + memoryObj.memory[i]);
+        console.log("result : " + memoryObj.resultMemory[i]);
     }
 }
+
+
+getArea('circle', 2);
+getArea('rect', 2, 3);
+getArea('trapezoid', 10, 15, 12);
+getArea('circle', 2, 3);
+printExecutionSequence()
