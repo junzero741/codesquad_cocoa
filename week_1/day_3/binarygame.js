c6a84040e260510dd79f1885d7c5a9639f151dec
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-11-05 16:35:50 +0900

    3일차 이진수 게임 리팩토링

--- week_1/day_3/binarygame.js
+++ week_1/day_3/binarygame.js
@@ -1,32 +1,25 @@
 function solution(radix, digit, peopleNum, turn) {
-  
+
     let numberStackArr = [];
     let gildong = [];
-   
-    
-    
-        for(let i = 0; i.toString([radix]).length < digit; i++ ){
-            numberStackArr.push(i.toString([radix]));  
-        }
-        
-        let numberStackStr = numberStackArr.join();
-        let gameConsole = numberStackStr.split("");
-     
-        for(let i = 0; i < gameConsole.length; i++){
-            if(gameConsole[i] === ",") {
-               gameConsole.splice(i,1);
-            }
-        }
 
-        
-        for(let i = turn-1; i < gameConsole.length; i+= peopleNum) {
-           
-                gildong.push(gameConsole[i]);
-    
-        }
-        
-        console.log(gildong); 
-    } 
 
- 
-    solution(2,4,3,1);
+
+    for (let i = 0; i.toString([radix]).length < digit; i++) {
+        numberStackArr.push(i.toString([radix]));
+    }
+
+    let numberStackStr = numberStackArr.join(""); 
+    let gameConsole = numberStackStr.split("");
+
+    for (let i = turn - 1; i < gameConsole.length; i += peopleNum) {
+
+        gildong.push(gameConsole[i]);
+
+    }
+
+    console.log(gildong);
+}
+
+
+solution(2, 4, 3, 2);
