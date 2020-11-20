0bc7d0ece8c8d3e275af5d54a3c26f526f40b77e
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-11-20 18:04:19 +0900

    Completed : 캥거루 점프 (답안 참조)

--- algorithms/Number-Line-Jumps.js
+++ algorithms/Number-Line-Jumps.js
@@ -6,34 +6,24 @@ const x2 = 9419;
 const v2 = 8076;
 
 function kangaroo(x1, v1, x2, v2) {
-    
-    let x1JumpCnt = 0;
-    let x2JumpCnt = 0;
-    
 
-    if ((x1 < x2 && v1 < v2) || (x1 > x2 && v1 > v2)) {
+    if (v1 <= v2) {
         return "NO";
     }
 
+    while(true) {
+        if(x1 > x2) {
+            return "NO";
+        }
 
-    while (x1<x2 && x2 <= 10000) {
         x1 += v1;
-        x1JumpCnt++;
         x2 += v2;
-        x2JumpCnt++;
 
-        if (x1 === x2 && x1JumpCnt === x2JumpCnt) {
-            return "YES"
-        } else {
-            continue;
+        if(x1 === x2) {
+            return "YES";
         }
     }
-
-    
-            return "NO";
-        
-  
-
+}
 
 function main() {
     console.log(kangaroo(x1, v1, x2, v2));
