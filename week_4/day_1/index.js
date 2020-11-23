1e83eb32c1a730a365312d0b51a6a990ba907a3b
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-11-23 11:15:09 +0900

    Ing : created list component, 1 second delay event

---
+++ week_4/day_1/index.js
@@ -0,0 +1,28 @@
+const listTitle = document.querySelector(".listTitle"),
+    mainList = document.querySelector(".list");
+
+
+
+
+
+listTitle.addEventListener("mouseover", wow);
+
+
+
+
+function wow() {
+    const SECOND = 1000;
+    setTimeout(function () {
+        console.log("wow");
+    }, SECOND);
+}
+
+
+function renderList() {
+    const SECOND = 1000;
+    setTimeout(function () {
+        
+    })
+}
+
+
