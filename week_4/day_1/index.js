ad35108d0aafee8998687c6ed589b636943e2c37
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-11-23 12:13:44 +0900

    Ing : 리스트 팝업 구현
    * 마우스 치우면 리스트 사라지게 해야 함

--- week_4/day_1/index.js
+++ week_4/day_1/index.js
@@ -1,28 +1,40 @@
 const listTitle = document.querySelector(".listTitle"),
     mainList = document.querySelector(".list");
+   
 
 
+function handleEvent () {
 
-
-
-listTitle.addEventListener("mouseover", wow);
-
-
-
-
-function wow() {
-    const SECOND = 1000;
-    setTimeout(function () {
-        console.log("wow");
-    }, SECOND);
 }
+
+
+listTitle.appendChild(mainList);
+listTitle.addEventListener("mouseover", renderList);
+
+
+// function consoleTest() {
+//     const SECOND = 1000;
+//     setTimeout(function () {
+//         console.log("wow");
+//     }, SECOND);
+// }
 
 
 function renderList() {
     const SECOND = 1000;
     setTimeout(function () {
-        
-    })
+        mainList.classList.remove("hiding");
+        console.log("마우스가 주황색 위에서 노는중");
+    }, SECOND);
+   
+
+   
+    
+ 
 }
 
+
+// function hideList() {
+//     mainList.classList.add("hiding");
+// }
 
