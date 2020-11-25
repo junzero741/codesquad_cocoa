d5500a25b8dec239af3170ea3f4a40919778394d
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-11-25 15:48:03 +0900

    add : 마을 위치와 크기 랜덤 지정

--- week_4/day_3/redMailBox/index.js
+++ week_4/day_3/redMailBox/index.js
@@ -2,27 +2,36 @@ const map = document.querySelector(".map");
      
 window.onload = createTown();
 
+
 function createTown() {
     const town = document.createElement("div");
     town.className = "town";
     map.appendChild(town);
-    
-    town.style.position = "absolute";
-    town.style.top = "10px";
-    town.style.left = "10px";
-    town.style.width = "10px";
-    town.style.height = "10px";
-    town.style.border = "2px solid black";
-
+    setTownFeature(town);
 } 
 
 
-
-function createCount(target) {
-    const el = document.createElement('div');   // div 태그 생성
-    el.className = target.innerText;            // 태그의 클래스 이름을 마우스가 위치한 과일의 내부 텍스트와 같게하기
-    el.innerText = `${el.className} : 1`;       // 태그의 내부 텍스트를  `클래스 이름 : 1` 로
-    fruitCount.appendChild(el);                 // fruitCount 에 자식 노드로 붙이기
+function setTownFeature(town) {
+    town.style.position = "absolute";
+    town.style.top = `${getRandomNum("pos")}px`;
+    town.style.left = `${getRandomNum("pos")}px`;
+    town.style.width =`${getRandomNum("size")}px`;
+    town.style.height = `${getRandomNum("size")}px`;
 }
 
+
+function getRandomNum(type) {
+    let randomNum;
+    const MAXPOS = 400;
+    const MINPOS = 0;
+    const MAXSIZE = 400;
+    const MINSIZE = 30;
+    if(type === "pos") {
+        randomNum = Math.random() * (MAXPOS - MINPOS) + MINPOS;
+    }
+    if(type === "size") {
+        randomNum = Math.random() * (MAXSIZE - MINSIZE) + MINSIZE;
+    }
+    return randomNum;
+}
 
