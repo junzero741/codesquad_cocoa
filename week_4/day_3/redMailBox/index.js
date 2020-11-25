722a2c0876777b92f59d4c3c48be70a103987220
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-11-25 15:58:03 +0900

    add: 마을 개수 랜덤 설정

--- week_4/day_3/redMailBox/index.js
+++ week_4/day_3/redMailBox/index.js
@@ -1,6 +1,4 @@
 const map = document.querySelector(".map");
-     
-window.onload = createTown();
 
 
 function createTown() {
@@ -8,14 +6,14 @@ function createTown() {
     town.className = "town";
     map.appendChild(town);
     setTownFeature(town);
-} 
+}
 
 
 function setTownFeature(town) {
     town.style.position = "absolute";
     town.style.top = `${getRandomNum("pos")}px`;
     town.style.left = `${getRandomNum("pos")}px`;
-    town.style.width =`${getRandomNum("size")}px`;
+    town.style.width = `${getRandomNum("size")}px`;
     town.style.height = `${getRandomNum("size")}px`;
 }
 
@@ -26,12 +24,28 @@ function getRandomNum(type) {
     const MINPOS = 0;
     const MAXSIZE = 400;
     const MINSIZE = 30;
-    if(type === "pos") {
+    if (type === "pos") {
         randomNum = Math.random() * (MAXPOS - MINPOS) + MINPOS;
     }
-    if(type === "size") {
+    if (type === "size") {
         randomNum = Math.random() * (MAXSIZE - MINSIZE) + MINSIZE;
     }
     return randomNum;
 }
 
+
+function getNumOfTowns() {
+    const MIN = 1;
+    const MAX = 26;
+    let randomNum = Math.random() * (MAX - MIN) + MIN;
+    return randomNum;
+}
+
+function init() {
+    const numOfTowns = getNumOfTowns();
+    for (let i = 0; i < numOfTowns; i++) {
+        window.onload = createTown();
+    }
+}
+
+init();
