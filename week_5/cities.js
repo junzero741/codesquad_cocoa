9b5fc1ad7e1166617eea462644b6a02fb7d77b53
DESKTOP-O9GP521\J <junzero741@gmail.com>
2020-12-01 15:07:56 +0900

    fix : 도시 코드 순서 수정

--- week_5/cities.js
+++ week_5/cities.js
@@ -20,25 +20,25 @@ function SpecialLand(name, fee, effect, idx) {
 
 // 도시 목록
 let start = new SpecialLand("start", 0, "getSalary", 0);                                    // [0]
-let taipei = new City("taipei", "none", "none", 0, 1);                                     // [1]                            
-let hongkong = new City("hongkong", "none", "none", 0, 3);                                 // [2]
-let maynila = new City("maynila", "none", "none", 0, 4);                                   // [3]
-let singapore = new City("singapore", "none", "none", 0, 6);                               // [4]                         
-let istanbul = new City("istanbul", "none", "none", 0, 9);                                 // [5]
-let uninhabitedIsland = new SpecialLand("uninhabitedIsland", 0, "delay3turn", 10);         // [6]
-let copenhagen = new City("copenhagen", "none", "none", 0, 13);                            // [7]
-let zurich = new City("zurich", "none", "none", 0, 16);                                    // [8]    
-let berilin = new City("berlin", "none", "none", 0, 18);                                   // [9]
-let getSocialWelfareFund = new SpecialLand("getSocialWelfareFund", 0, "getAllFund", 20);   // [10] 
-let sydney = new City("sydney", "none", "none", 0, 24);                                    // [11]
-let lisbon = new City("lisbon", "none", "none", 0, 27);                                    // [12]
-let madrid = new City("madrid", "none", "none", 0, 29);                                    // [13]
-let spaceStation = new SpecialLand("spaceStation", 0, "goAnywhere", 0, 30);                // [14]
-let paris = new City("paris", "none", "none", 0, 33);                                      // [15]
-let roma = new City("roma", "none", "none", 0, 34);                                        // [16]  
-let london = new City("london", "none", "none", 0, 36);                                    // [17]
-let newyork = new City("newyork", "none", "none", 0, 37);                                  // [18]
-let paySocialWelfareFund = new SpecialLand("paySocialWelfareFund", 0, "payFund", 38);      // [19]
+let taipei = new City("taipei", "none", "none", 0, 1);                                      // [1]                            
+let hongkong = new City("hongkong", "none", "none", 0, 2);                                  // [2]
+let maynila = new City("maynila", "none", "none", 0, 3);                                    // [3]
+let singapore = new City("singapore", "none", "none", 0, 4);                                // [4]                         
+let uninhabitedIsland = new SpecialLand("uninhabitedIsland", 0, "delay3turn", 6);           // [5]
+let istanbul = new City("istanbul", "none", "none", 0, 5);                                  // [6]
+let copenhagen = new City("copenhagen", "none", "none", 0, 7);                              // [7]
+let zurich = new City("zurich", "none", "none", 0, 8);                                      // [8]    
+let berlin = new City("berlin", "none", "none", 0, 9);                                      // [9]
+let getSocialWelfareFund = new SpecialLand("getSocialWelfareFund", 0, "getAllFund", 10);    // [10] 
+let sydney = new City("sydney", "none", "none", 0, 11);                                     // [11]
+let lisbon = new City("lisbon", "none", "none", 0, 12);                                     // [12]
+let madrid = new City("madrid", "none", "none", 0, 13);                                     // [13]
+let paris = new City("paris", "none", "none", 0, 15);                                       // [14]
+let spaceStation = new SpecialLand("spaceStation", 0, "goAnywhere", 0, 14);                 // [15]
+let roma = new City("roma", "none", "none", 0, 16);                                         // [16]  
+let london = new City("london", "none", "none", 0, 17);                                     // [17]
+let newyork = new City("newyork", "none", "none", 0, 18);                                   // [18]
+let paySocialWelfareFund = new SpecialLand("paySocialWelfareFund", 0, "payFund", 19);       // [19]
 
 
 
@@ -55,10 +55,10 @@ uninhabitedIsland.freeze = function (player) {
 
 
 // 보드판 안에 도시들 삽입
-const board = [start, taipei, hongkong, maynila, singapore, istanbul, uninhabitedIsland,
-    copenhagen, zurich, berilin, getSocialWelfareFund,
-    sydney, lisbon, madrid, spaceStation,
-    paris, roma, london, newyork, paySocialWelfareFund];
+const board = [start, taipei, hongkong, maynila, singapore, uninhabitedIsland,
+                istanbul, copenhagen, zurich, berlin, getSocialWelfareFund,
+                sydney, lisbon, madrid,  paris, spaceStation,
+                roma, london, newyork, paySocialWelfareFund];
 
 
 
